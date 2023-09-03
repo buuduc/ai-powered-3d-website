2fd8eb14bc8a52f203b32c76ba223492c7a21ed3
buuduc <buuduc1999@gmail.com>
2023-09-04 00:30:10 +0700

    NEW: done backend and add env for fe

--- client/src/components/AiPicker.jsx
+++ client/src/components/AiPicker.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import CustomButton from './CustomButton'
 
-const AiPicker = () => {
+const AiPicker = ({
+  prompt,
+setPrompt,
+generatingImg,
+handleSubmit,
+}) => {
   return (
-    <div>AiPicker</div>
+    <div className='aipicker-container'>
+      <textarea 
+      placeholder='Ask Ai...'
+      rows={5}
+      value={prompt}
+      onChange={(e) => { setPrompt(e.target.value) }}
+      className='aipicker-textarea'
+      />
+      <div className='flex flex-wrap gap-3'>
+        {
+          generatingImg
+            ? <CustomButton
+              type='outline'
+              title='Asking AI'
+              customStyles='text-xs'
+            />
+            : <>
+              <CustomButton
+                type='outline'
+                title='AI Logo'
+                customStyles='text-xs'
+                handleClick={() => { handleSubmit('logo') }}
+              />
+              <CustomButton
+                type='outline'
+                title='AI Full'
+                customStyles='text-xs'
+                handleClick={() => { handleSubmit('full') }}
+              />
+            </>
+        }
+      </div>
+    </div>
   )
 }
 
